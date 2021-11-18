68a962ac0618e693b736e550463498316266c238
Victroria Anisimova <vitkakim@mail.ru>
2021-11-18 23:44:28 +0300

    deleted comments in DayTwo

--- src/test/java/ru/netology/DayTwo.java
+++ src/test/java/ru/netology/DayTwo.java
@@ -23,18 +23,12 @@ public class DayTwo {
         day = $(".calendar__day_state_today").getText();
         month = $(".calendar__name").getText();
         today = day + " " + month;
-//        dayTwo = LocalDate.parse(today,
-//                DateTimeFormatter.ofPattern("dd MMMM yyyy").localizedBy(Locale.forLanguageTag("ru"))).format(format);
-
         SimpleDateFormat formatIn = new SimpleDateFormat("dd MMMM yyyy", Locale.forLanguageTag("ru"));
         Date instance = formatIn.parse(today);
         LocalDate localDate = Instant.ofEpochMilli(instance.getTime())
                 .atZone(ZoneId.systemDefault())
                 .toLocalDate();
-//        LocalDate localDate = LocalDate.parse( new SimpleDateFormat("dd.MM.yyyy").format(instance) );
-//        SimpleDateFormat formatOut = new SimpleDateFormat("dd.MM.yyyy");
         dayTwo = localDate.plusDays(7).format(format);
-//        System.out.println(localDate);
         return dayTwo;
     }
 
