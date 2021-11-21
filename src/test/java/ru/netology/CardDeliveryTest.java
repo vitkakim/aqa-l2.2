360f03ac436179867d23b6d97dbc077275b8b19f
Victroria Anisimova <vitkakim@mail.ru>
2021-11-21 18:45:08 +0300

    fixed test two

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,5 +1,7 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,11 +11,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
     String dayOne = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     DayTwo dayTwo = new DayTwo();
+
 
     @Test
     void shouldBeTestForFirstHW() {
@@ -30,19 +34,21 @@ public class CardDeliveryTest {
     }
 
     @Test
-    void shouldBeTestForSecondHW() throws ParseException {
+    void shouldBeTestForSecondHW(){
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Са");
         $$(".menu-item").find(exactText("Самара")).click();
         $(".input__icon").click();
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(dayTwo.getDayTwo());
+        while ($(byAttribute("data-day", dayTwo.getDayTwo())).isDisplayed() == false) {
+            $("[data-step='1']").click();
+        }
+        $(byAttribute("data-day", dayTwo.getDayTwo())).click();
         $("[data-test-id='name'] input").setValue("Анисимова Виктория Эдуардовна");
         $("[data-test-id='phone'] input").setValue("+12345678901");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $$("button").find(exactText("Забронировать")).click();
         $(".notification_visible[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='notification']").shouldHave(text(dayTwo.getDayTwo()));
+        $("[data-test-id='notification']").shouldHave(text($("[data-test-id='date'] input").getAttribute("value")));
     }
 
 }
--- src/test/java/ru/netology/DayTwo.java
+++ src/test/java/ru/netology/DayTwo.java
@@ -1,34 +1,17 @@
 package ru.netology;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Locale;
-
-import static com.codeborne.selenide.Selenide.$;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 
 public class DayTwo {
     private String dayTwo;
-    private String day;
-    private String month;
-    private String today;
-    private DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-
-    public String getDayTwo() throws ParseException {
-        day = $(".calendar__day_state_today").getText();
-        month = $(".calendar__name").getText();
-        today = day + " " + month;
-        SimpleDateFormat formatIn = new SimpleDateFormat("dd MMMM yyyy", Locale.forLanguageTag("ru"));
-        Date instance = formatIn.parse(today);
-        LocalDate localDate = Instant.ofEpochMilli(instance.getTime())
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
-        dayTwo = localDate.plusDays(7).format(format);
+    public String getDayTwo() {
+        LocalDateTime day1 = LocalDateTime.of(LocalDate.now().plusDays(7), LocalTime.of(15, 00, 00));
+        day1.toEpochSecond(ZoneOffset.MAX);
+        dayTwo = String.valueOf(day1.toEpochSecond(ZoneOffset.MAX) * 1000);
         return dayTwo;
     }
 
